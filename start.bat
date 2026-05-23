5e19fdee7b199e32391deccf19f3b8c6202eb29c
remaker-tech <resulovadem333@gmail.com>
2026-05-23 23:32:13 +0400

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,7 @@
+@off
+echo E-Qaime Parser Isə Salinir...
+echo -----------------------------------
+python yep.py
+echo -----------------------------------
+echo Is bitdi! Excel fayli yaradildi.
+pause
