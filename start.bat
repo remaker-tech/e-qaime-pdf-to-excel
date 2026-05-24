cacbfb688f5d47dff9117eae76bbfe4bcf86db62
remaker-tech <resulovadem333@gmail.com>
2026-05-24 14:12:14 +0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,5 @@
 @echo off
-echo E-Qaime Parser Isə Salinir...
 echo -----------------------------------
 python yep.py
 echo -----------------------------------
-echo Is bitdi! Excel fayli yaradildi.
 pause
